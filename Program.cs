472fad067596cbee136b2a7ffe5fb403a152b81d
usmanshamsi <usman.shamsi@live.com>
2021-12-25 13:31:03 +0500

    program created and added to repo - first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ush_lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is for testing of library functions only
+            // this is not a main application
+
+
+        }
+    }
+}
